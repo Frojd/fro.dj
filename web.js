d7ae483f16f9865af2c975c14c3383d1060081af
Andreas Hultgren <andreas.hultgren@frojd.se>
2012-08-15 14:11:20 +0200

    Added database setup to web.js for testing on heroku

--- web.js
+++ web.js
@@ -2,9 +2,22 @@ var http = require('http'),
 		path = require('path'),
 		// node_modules
 		tako = require('tako'),
+		mongo = require('mongodb'),
 		// variables
   	app = tako(),
 		port = process.env.PORT || 5000;
+
+/* Set up mongo */
+
+var dbhost = process.env['MONGO_NODE_DRIVER_HOST'] || 'localhost',
+	dbport = process.env['MONGO_NODE_DRIVER_PORT'] || 27017;
+
+console.log("Connecting to " + dbhost + ":" + dbport);
+
+var db = new mongo.Db('frodj', new mongo.Server(dbhost, dbport, {auto_reconnect: true})),
+	alias = require('./alias.js')({db: db});
+
+/* Route */
 
 app.route('/static/*').files(path.join(__dirname, 'static'))
 
@@ -12,4 +25,4 @@ app.route('/')
 	.file('html/index.html')
   .methods('GET');
 
-app.httpServer.listen(port)
+app.httpServer.listen(port);
